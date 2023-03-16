19cdb2545e83e06bffb8ae6a7dd4a6ad5628f222
Replit user <>
2023-03-16 16:15:45 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -27,11 +27,14 @@ almacena en las variables apuntadas por max y min.
 void arrayMaxMin(int *a, int n, int *max, int *min) {
   *max = a[0];
   *min = a[0];
-  for (int i = 1; i < n; i++){
-    if (a[i] > *max){
+  for (int i = 1; i < n; i++)
+  {
+    if (a[i] > *max)
+    {
       *max = a[i];
     }
-    if (a[i] < *min){
+    if (a[i] < *min)
+    {
       *min = a[i];
     } 
   }
@@ -78,7 +81,6 @@ typedef struct {
 } Vector;
 
 Vector * crearVector(int n) {
-
 }
 
 /*
